89727af5771dfa62bb6e2f1370448f0b3f05a568
evpobr <evpobr@gmail.com>
2017-08-17 14:04:01 +0500

    Fix broken function declaration

--- FB_2_txt/save_txt_dialog.pas
+++ FB_2_txt/save_txt_dialog.pas
@@ -426,7 +426,7 @@ begin
   { Hide any hint windows left behind }
   Application.HideHint;
 end;
-function TOpenPictureDialog.Execute;
+function TOpenPictureDialog.Execute: Boolean;
 Var
   Reg:TRegistry;
 begin
